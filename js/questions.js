828f41fefc53b51280e036041a14a0f9cc53ad44
Jacob <jakebauer21@gmail.com>
2023-11-17 11:52:10 -0600

    enlarged start button and took rounded edges with border-radius

--- js/questions.js
+++ js/questions.js
@@ -3,17 +3,6 @@
 let questions = [
 	{
 		numb: 1,
-		question: 'Insert Qs',
-		answers: [
-			'Insert answer 1',
-			'Insert answer 2',
-			'Insert answer 3',
-			'Insert answer 4',
-		],
-		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-	},
-	{
-		numb: 2,
 		question: 'Insert 1 Qs',
 		answers: [
 			'Insert answer 1',
@@ -24,7 +13,7 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 3,
+		numb: 2,
 		question: 'Insert 2 Qs',
 		answers: [
 			'Insert answer 1',
@@ -35,7 +24,7 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 4,
+		numb: 3,
 		question: 'Insert 3 Qs',
 		answers: [
 			'Insert answer 1',
@@ -46,7 +35,7 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 5,
+		numb: 4,
 		question: 'Insert 4 Qs',
 		answers: [
 			'Insert answer 1',
@@ -57,7 +46,7 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 6,
+		numb: 5,
 		question: 'Insert 5 Qs',
 		answers: [
 			'Insert answer 1',
@@ -68,8 +57,8 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 7,
-		question: 'Insert 6  Qs',
+		numb: 6,
+		question: 'Insert 6 Qs',
 		answers: [
 			'Insert answer 1',
 			'Insert answer 2',
@@ -79,7 +68,7 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 8,
+		numb: 7,
 		question: 'Insert 7 Qs',
 		answers: [
 			'Insert answer 1',
@@ -90,7 +79,7 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 9,
+		numb: 8,
 		question: 'Insert 8 Qs',
 		answers: [
 			'Insert answer 1',
@@ -101,8 +90,129 @@ let questions = [
 		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
 	},
 	{
-		numb: 10,
+		numb: 9,
 		question: 'Insert 9 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 10,
+		question: 'Insert 10 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 11,
+		question: 'Insert 11 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 12,
+		question: 'Insert 12 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 13,
+		question: 'Insert 13 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 14,
+		question: 'Insert 14 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 15,
+		question: 'Insert 15 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 16,
+		question: 'Insert 16 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 17,
+		question: 'Insert 17  Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 18,
+		question: 'Insert 18 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 19,
+		question: 'Insert 19 Qs',
+		answers: [
+			'Insert answer 1',
+			'Insert answer 2',
+			'Insert answer 3',
+			'Insert answer 4',
+		],
+		options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+	},
+	{
+		numb: 20,
+		question: 'Insert 20 Qs',
 		answers: [
 			'Insert answer 1',
 			'Insert answer 2',
